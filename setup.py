1d0209ceed9b2653284833ba96be37e2e6a1ab69
Eddy Hintze <eddy.hintze@gmail.com>
2015-12-11 16:16:15 -0500

    Updated version and url

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 setup(
     name='custom_utils',
     packages=['custom_utils'],
-    version='0.0.1a',
+    version='0.2.0',
     description='Custom Utility and helper functions',
     author='Eddy Hintze',
     author_email="eddy.hintze@gmail.com",
-    url="https://xnet.serverx.co/git/",
+    url="https://git.eddyhintze.com/xtream1101/custom-utils",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
