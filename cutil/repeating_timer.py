74cccd7f9246b13be8ec5136d490e5953a587e56
Eddy Hintze <eddy.hintze@gmail.com>
2016-09-21 11:04:32 -0400

    Added max_tries to timer

--- cutil/repeating_timer.py
+++ cutil/repeating_timer.py
@@ -3,19 +3,22 @@ from threading import Timer
 
 class RepeatingTimer():
 
-    def __init__(self, interval, func, repeat=True, *args, **kwargs):
+    def __init__(self, interval, func, repeat=True, max_tries=None, *args, **kwargs):
         self.interval = interval
         self.func = func
         self.repeat = repeat
+        self.max_tries = max_tries
+        self.try_count = 0
         self.args = args
         self.kwargs = kwargs
 
         self.timer = None
 
     def _callback(self):
+        self.try_count += 1
         self.func(*self.args, **self.kwargs)
 
-        if self.repeat is True:
+        if self.repeat is True and self.max_tries is not None and self.try_count < self.max_tries:
             self.start()
         else:
             self.cancel()
